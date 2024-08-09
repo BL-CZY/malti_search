7dc0cd1eb98cf9e3977e07ecfc925cd14847158f
BL-CZY <jiguangllsw@gmail.com>
2024-08-08 20:45:46 -0700

    little improvement

--- src/db/fetch.rs
+++ src/db/fetch.rs
@@ -7,23 +7,15 @@ use serde_json;
 
 #[napi]
 pub fn get_word(word: String) -> String {
-    println!("get word: {}", word);
+    println!("getting word: {}", word);
     let db: Database = CLIENT.database("local");
     let col: Collection<Document> = db.collection("words");
 
-    match col.find_one(doc! {"word": &word}, None) {
-        Ok(res) => {
-            if let Some(doc) = res {
-                println!("found word {}", word);
-                serde_json::to_string_pretty(&doc).unwrap()
-            } else {
-                println!("found word {}, but the value is corrupted", word);
-                "".into()
-            }
-        }
-        Err(_) => {
-            println!("can't find word {}", word);
-            "".into()
-        }
+    if let Ok(Some(res)) = col.find_one(doc! {"word": &word}, None) {
+        println!("found word {}", word);
+        serde_json::to_string_pretty(&res).unwrap()
+    } else {
+        println!("can't find it");
+        "".into()
     }
 }
